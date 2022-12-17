d04855344970d714e97b8a8b18d532acc4bb701c
Daniel <dan_santtos@live.com>
2022-12-17 12:21:41 -0300

    Atualização arquivo script.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,50 @@ let secondPlayer;
 
 let player1 = 0;
 let player2 = 0;
+
+// adicionando o evento de click aos boxes
+for(let i = 0; i < boxes.length; i++) {
+    
+    // quando alguem clica na caixa
+    boxes[i].addEventListener("click", function() {
+
+        let el = checkEl(player1, player2);
+
+        // verifica se ja tem x ou o
+
+        if(this.childNodes.length == 0) {
+        
+            let cloneEl = el.cloneNode(true);
+
+        this.appendChild(cloneEl);
+
+        // computar jogada
+        if(player1 == player2) {
+            
+            player1++;
+
+        } else {
+
+            player2++;
+
+        }
+          
+        }
+
+    });
+
+}
+
+// ve quem vai jogar
+function checkEl(player1, player2) {
+
+    if(player1 == player2) {
+        // x
+        el = x;
+    } else {
+        // o
+        el = o;
+    }
+
+    return el;
+}
