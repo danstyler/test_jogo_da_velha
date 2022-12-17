26c12f1cdd9c44217a0e382d9eb9452b51afa90b
Daniel <dan_santtos@live.com>
2022-12-17 18:26:11 -0300

    Atualização arquivo script.js

--- js/scripts.js
+++ js/scripts.js
@@ -30,18 +30,46 @@ for(let i = 0; i < boxes.length; i++) {
         // computar jogada
         if(player1 == player2) {
             player1++;
+
+            if(secondPlayer == 'ai-player') {
+
+                // funcao executar a jogada
+                computerPlay();
+                player2++;
+
+
+            }
         } else {
             player2++;
         }
 
         // checa quem venceu
-
         checkWinCondition();
           
         }
 
     });
 
+}
+
+// evento para seaber se é 2 players ou IA
+
+for(let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function() {
+
+        secondPlayer = this.getAttribute("id");
+
+        for(j = 0; j < buttons.length; j++) {
+            buttons[j].style.display = 'none';
+        }
+
+        setTimeout(function(){
+            let container = document.querySelector("#container");
+            container.classList.remove("hide");
+
+        },500)
+    })
 }
 
 // ver quem vai jogar
@@ -58,9 +86,7 @@ function checkEl(player1, player2) {
     return el;
 }
 
-
 // ver quem ganhou
-
 checkWinCondition = () => {
 
     let b1 = document.getElementById("block-1");
@@ -256,3 +282,33 @@ function declareWinner(winner) {
     }
 }
 
+// executar a lógica da jogada do CPU
+function computerPlay() {
+
+    let cloneO = o.cloneNode(true);
+    counter = 0;
+    filled = 0;
+
+    for(let i = 0; i < boxes.length; i++) {
+
+        let randomNumber = Math.floor(Math.random() * 5);
+
+        // só preencher se estiver vazio o filho
+        if(boxes[i].childNodes[0] == undefined) {
+            if(randomNumber <= 1) {
+                boxes[i].appendChild(cloneO);
+                counter++;
+                break;
+            }
+            // checagem de quantas estão preenchidas
+        } else {
+            filled++;
+        }
+
+    }
+
+    if(counter == 0 && filled < 9) {
+        computerPlay();
+    }
+
+}
